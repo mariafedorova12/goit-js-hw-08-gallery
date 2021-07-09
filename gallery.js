8fe300186eade0d5a0410472d79e8a586aad186d
mariafedorova12 <mariafedorova12@users.noreply.github.com>
2021-07-10 00:15:18 +0200

    some changes + modal wnd

--- gallery.js
+++ gallery.js
@@ -20,4 +20,40 @@ function createGallery(cards) {
  
   // console.log(markup)
 }
+ 
+galleryBox.addEventListener('click', onGalleryBoxClick)
 
+const modalOpen = document.querySelector('.lightbox');
+const lightboxImg = document.querySelector('.lightbox__image')
+
+function onGalleryBoxClick(e) {
+
+  if (!e.target.classList.contains('gallery__image')) {
+    return
+  } else {
+
+    modalOpen.classList.add('is-open');
+
+    lightboxImg.src = e.target.dataset.source;
+    lightboxImg.alt = e.target.alt;
+  }
+}
+
+function closeModal(e) {
+  modalOpen.classList.remove('is-open');
+
+  lightboxImg.src = "";
+  lightboxImg.alt = "";
+}
+
+const buttonClose = document.querySelector('.lightbox__button');
+// console.log(buttonClose);
+
+buttonClose.addEventListener('click', closeModal);
+
+// document.addEventListener('keydown', function(e) {
+//   if (e.key === 'Escape') {
+ 
+//   hideModal()
+//   }
+//   });
